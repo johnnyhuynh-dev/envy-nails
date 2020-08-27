1405cba1b357c964075dc08541d75d9f219689ca
Johnny Huynh <huynhtuandat.ueh@gmail.com>
2020-08-28 00:17:27 +1000

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="address">Shop 3/113-115 Main Rd, Moonah, TAS 7009</div>
+      <div className="phone">0432 235 635</div>
+      <div className="social-media">
+        <i className="fab fa-facebook-f"></i>
+        <i className="fab fa-instagram"> </i>
+      </div>
+    </div>
+  );
+}
