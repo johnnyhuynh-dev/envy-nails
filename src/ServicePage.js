bb756d663dcd3e265f00ff5a44310e7abb773f4a
Johnny Huynh <huynhtuandat.ueh@gmail.com>
2020-09-11 17:24:47 +1000

    configure firebase init, build version, modify le

--- src/ServicePage.js
+++ src/ServicePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ImageDemo from "./ImageDemo";
 import Loader from "./Loader";
 import price from "./price.json";
